54509b206ea22bbc46a3c990ada68c58cc1743f7
Mher <mher.simonyan.2014@gmail.com>
2020-08-06 14:36:51 +0400

    Added new step

--- src/components/HostHome.jsx
+++ src/components/HostHome.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import HostHomeStep from './HostHomeStep';
 
 export default function HostHome(props) {
+  useEffect(() => {
+    setInterval(() => {
+      console.log(options);
+    }, 3000);
+  }, [])
   const [step, setStep] = useState(1);
   const images = [];
+  const [options] = useState({});
+  const setOption = (keys, values) => {
+    for (let i = 0; i < keys.length; i++) {
+      options[keys[i]] = values[i];
+    }
+  }
 
   return (
-    <HostHomeStep step={1} />
+    <HostHomeStep step={step} setStep={setStep} setOption={setOption} />
   )
 }
--- src/components/HostHomeStep.jsx
+++ src/components/HostHomeStep.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
-import { Select, MenuItem, InputLabel, makeStyles, FormControl, FormHelperText } from "@material-ui/core";
+import {
+  Select,
+  MenuItem,
+  InputLabel,
+  makeStyles,
+  FormControl,
+  FormHelperText,
+  Button,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -14,17 +22,21 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const HostHomeStepCmp = styled.div`
-  display: flex;
   padding: 0 100px;
   margin-top: 50px;
 `;
 
-const HostHomeStepText = styled.div`
-  flex: 1;
+const StyledForm = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 `;
 
-const HostHomeStepImg = styled.div`
-  flex: 1;
+const StyledButton = styled(Button)`
+  width: 125px;
+`;
+
+const HostHomeStepText = styled.div`
+  color: #364f6b;
 `;
 
 export default function HostHomeStep(props) {
@@ -39,53 +51,69 @@ export default function HostHomeStep(props) {
     returnedJSX = (
       <HostHomeStepCmp>
         <HostHomeStepText>
-          <h3>
+          <h3 style={{marginTop: 10}}>
             Hi, {user ? user.email : "Stranger"}! You're welcome in our
             travelling site. Let's get started
           </h3>
-          <h4>STEP 1</h4>
-          <h2>What kind of place do you have?</h2>
-          <FormControl required className={classes.formControl}>
-            <InputLabel>City</InputLabel>
-            <Select
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              className={classes.selectEmpty}
-            >
-              <MenuItem value="yerevan">Yerevan</MenuItem>
-            </Select>
-            <FormHelperText>Required</FormHelperText>
-          </FormControl>
-          <FormControl required className={classes.formControl}>
-            <InputLabel>District</InputLabel>
-            <Select
-              value={district}
-              onChange={(e) => setDistrict(e.target.value)}
-              className={classes.selectEmpty}
-            >
-              <MenuItem value="arabkir">Arabkir</MenuItem>
-              <MenuItem value="bangladesh">Bangladesh</MenuItem>
-              <MenuItem value="shengavit">Shengavit</MenuItem>
-              <MenuItem value="ajapnyak">Ajapnyak</MenuItem>
-            </Select>
-            <FormHelperText>Required</FormHelperText>
-          </FormControl>
-          <FormControl required className={classes.formControl}>
-            <InputLabel>Guests</InputLabel>
-            <Select
-              value={guests}
-              onChange={(e) => setGuests(e.target.value)}
-              className={classes.selectEmpty}
-            >
-              {[1,2,3,4,5,6,7,8,9,10].map(guestsNumber => (
-                <MenuItem key={guestsNumber} value={guestsNumber}>for {guestsNumber} guests</MenuItem>
-              ))}
-            </Select>
-            <FormHelperText>Required</FormHelperText>
-          </FormControl>
+          <h4 style={{marginTop: 10}}>STEP {props.step}</h4>
+          <h2 style={{marginTop: 10}}>What kind of place do you have?</h2>
+          <StyledForm>
+            <FormControl required className={classes.formControl}>
+              <InputLabel>City</InputLabel>
+              <Select
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                className={classes.selectEmpty}
+              >
+                <MenuItem value="yerevan">Yerevan</MenuItem>
+              </Select>
+              <FormHelperText>Required</FormHelperText>
+            </FormControl>
+            <FormControl required className={classes.formControl}>
+              <InputLabel>District</InputLabel>
+              <Select
+                value={district}
+                onChange={(e) => setDistrict(e.target.value)}
+                className={classes.selectEmpty}
+              >
+                <MenuItem value="arabkir">Arabkir</MenuItem>
+                <MenuItem value="bangladesh">Bangladesh</MenuItem>
+                <MenuItem value="shengavit">Shengavit</MenuItem>
+                <MenuItem value="ajapnyak">Ajapnyak</MenuItem>
+              </Select>
+              <FormHelperText>Required</FormHelperText>
+            </FormControl>
+            <FormControl required className={classes.formControl}>
+              <InputLabel>Guests</InputLabel>
+              <Select
+                value={guests}
+                onChange={(e) => setGuests(e.target.value)}
+                className={classes.selectEmpty}
+              >
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((guestsNumber) => (
+                  <MenuItem key={guestsNumber} value={guestsNumber}>
+                    for {guestsNumber} guests
+                  </MenuItem>
+                ))}
+              </Select>
+              <FormHelperText>Required</FormHelperText>
+            </FormControl>
+            <div style={{ display: "flex", justifyContent: "flex-end", gridColumnStart: 3 }}>
+              <StyledButton
+                style={{ backgroundColor: "#364F6B" }}
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={() => {
+                  props.setStep(props.step + 1);
+                  props.setOption(["city", "district", "guests"], [city, district, guests]);
+                }}
+              >
+                NEXT
+              </StyledButton>
+            </div>
+          </StyledForm>
         </HostHomeStepText>
-        <HostHomeStepImg>
-        </HostHomeStepImg>
       </HostHomeStepCmp>
     );
   }
