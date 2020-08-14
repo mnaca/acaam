585bcf8c9caeb5f216b8b35f8af0822a46d9c97a
andoharutyunyan <Andochaxkal1990@gmail.com>
2020-08-14 13:34:13 +0400

    Added Profile Page

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -5,7 +5,25 @@ import styled from "styled-components";
 import { useSelector } from "react-redux";
 
 const ProfilePageCmp = styled.div`
+  display:flex;
   padding: 20px;
+`;
+const ImgStyled= styled.div`
+cursor: pointer;
+color: #364f6b;
+padding: 10px;
+border-radius: 5px;
+box-shadow: 0px 0px 10px 1px rgba(93, 120, 148, 1);
+height: 300px;
+width:250px;
+`;
+const NameDiv=styled.div`
+font-weight:bold;
+font-size: 25px;
+`;
+const ProfilInfo=styled.div`
+margin-left:50px;
+color: #364f6b;
 `;
 
 export default function ProfilePage(props) {
@@ -13,6 +31,7 @@ export default function ProfilePage(props) {
   const [user, setUser] = useState(null);
   const userInfo = useSelector((state) => state.userInfo);
   const [profileImage, setProfileImage] = useState(null);
+  const [selfDescription, setSelfDescription] = useState("");
 
   useEffect(() => {
     if (auth.currentUser || userInfo) {
@@ -30,14 +49,38 @@ export default function ProfilePage(props) {
           profileImagesRef.getDownloadURL().then((url) => setProfileImage(url));
         });
     }
-  }, [userId, userInfo]);
+  }, [userId, userInfo,user]);
 
   return (
     <>
       {user ? (
-        <ProfilePageCmp>
-          {user.firstName + user.lastName}
-          <img src={profileImage} alt="" />
+        <ProfilePageCmp>           
+         <ImgStyled><img style={{height: '200px',width:'100%'}} src={profileImage} alt="" />
+         <div><b>Email:</b> {user.mail}</div>
+         <div><b>Tel:</b>{user.tel} </div> 
+         </ImgStyled>
+         <ProfilInfo>
+         <NameDiv>Hi I'm {user.firstName +' '+ user.lastName}</NameDiv>  
+         <textarea
+          value={selfDescription}
+          onChange={(e) => setSelfDescription(e.target.value)}
+          style={{
+            borderRadius: "5px",
+            marginTop: 15,
+            borderColor: "#364f6b",
+            width: "100%",
+            minWidth:600,
+            minHeight: 250,
+            maxHeight: 600,
+            resize: "vertical",
+            padding: "10px",
+            color: "#364f6b",
+            fontSize: 16,
+          }}
+          placeholder="Say something about you ..."
+        ></textarea>
+         </ProfilInfo>  
+                  
         </ProfilePageCmp>
       ) : null}
     </>
